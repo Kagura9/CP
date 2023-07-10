8c2a38e3d0484d1f460b60077bbd9d4b686685f3
Kagura9 <july9999999999999@gmail.com>
2023-07-11 00:43:14 +0200

    test

---
+++ src/tests/domino.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class domino {
+    public static void main(String[] args)  {
+       int w = Integer.parseInt(args[0]) ;
+        int a = Integer.parseInt(args[1]) ;
+        System.out.println(a+w);
+    }
+}
